0d4f5010b33295fd4f27b0cbcbbd9b6abfdaf657
zhonglive <innovateandworld@163.com>
2021-02-03 22:19:27 +0800

    全局挂载echarts

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 将全局echarts对象挂载到Vue原型对象上
+Vue.prototype.$echarts = window.echarts
+
 createApp(App).use(store).use(router).mount('#app')
