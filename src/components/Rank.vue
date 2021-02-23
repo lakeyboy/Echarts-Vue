00c34888dace2b17f9a63a25aeb19c5657b4fd68
zhonglive <innovateandworld@163.com>
2021-02-23 15:18:52 +0800

    1.销量排行完

--- src/components/Rank.vue
+++ src/components/Rank.vue
@@ -1,7 +1,6 @@
 <template>
   <!-- 模板中 最外层只有 -->
   <div class="com-container">
-    255
     <!-- 显示图表的div对象 -->
     <div class="com-chart" ref="rank_ref"></div>
   </div>
@@ -12,7 +11,10 @@ export default {
   data() {
     return {
       chartInstance: null,
-      allData: null
+      allData: null,
+      startValue: 0, //区域缩放的起点值
+      endValue: 9, //区域缩放的终点值
+      timerId: null
     }
   },
   //生命周期函数，组件创建时执行
@@ -27,27 +29,72 @@ export default {
   //生命周期函数，组件销毁时执行
   destroyed() {
     window.removeEventListener('resize', this.screenAdapter)
+    clearInterval(this.timerId)
   },
   methods: {
     initChart() {
       //初始化后会返回一个实例对象
-      this.chartInstance = this.$echarts.init(this.$refs.rank_ref)
+      this.chartInstance = this.$echarts.init(this.$refs.rank_ref, 'chalk')
       const initOption = {
+        title: {
+          text: '▍地区销售排行',
+          left: 20,
+          top: 20
+        },
+        //控制坐标轴大小
+        grid: {
+          top: '40%',
+          left: '5%',
+          right: '5%',
+          bottom: '5%',
+          containLabel: true
+        },
+        tooltip: {
+          show: true
+        },
         xAxis: {
           type: 'category'
-        }
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          //一个对象表示一组数据
+          {
+            type: 'bar'
+          }
+        ]
       }
       this.chartInstance.setOption(initOption)
+      this.chartInstance.on('mouseover', () => {
+        clearInterval(this.timerId)
+      })
+      this.chartInstance.on('mouseout', () => {
+        this.startInterval()
+      })
     },
     //获取数据
     async getData() {
       const { data: ret } = await this.$http.get('rank')
       this.allData = ret
 
+      //a和b相当于alldata中item,比较时是对比item.value
+      this.allData.sort((a, b) => {
+        return b.value - a.value
+      })
+      //更新数据显示图表
       this.updateChart()
+      //设置定时器
+      this.startInterval()
     },
     // 更新数据
     updateChart() {
+      // 渐变颜色数组
+      const colorArr = [
+        ['#0BA82C', '#4FF778'],
+        ['#2e72bf', '#23e5e5'],
+        ['#5052ee', '#ab6ee5']
+      ]
       // 省份数据  x轴
       const provinceArr = this.allData.map((item) => {
         return item.name
@@ -60,16 +107,84 @@ export default {
       const dataOption = {
         xAxis: {
           data: provinceArr
-        }
+        },
+        dataZoom: {
+          show: false,
+          startValue: this.startValue,
+          endValue: this.endValue
+        },
+        series: [
+          {
+            data: valueArr,
+            itemStyle: {
+              // arg表示每个柱的数值
+              color: (arg) => {
+                let targetColorArr = null
+                if (arg.value > 200) {
+                  targetColorArr = colorArr[0]
+                } else if (arg.value > 130) {
+                  targetColorArr = colorArr[1]
+                } else {
+                  targetColorArr = colorArr[2]
+                }
+
+                return new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  {
+                    offset: 0,
+                    color: targetColorArr[0]
+                  },
+                  {
+                    offset: 1,
+                    color: targetColorArr[1]
+                  }
+                ])
+              }
+            }
+          }
+        ]
       }
       this.chartInstance.setOption(dataOption)
     },
+
     //屏幕尺寸变化
     screenAdapter() {
-      const adapterOption = {}
+      const titleFontSize = (this.$refs.rank_ref.offsetWidth / 100) * 3.6
+
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: titleFontSize
+          }
+        },
+        series: [
+          {
+            barWidth: titleFontSize,
+            itemStyle: {
+              //分别是左上角，右上角，右下，左下
+              barBorderRadius: [titleFontSize / 2, titleFontSize / 2, 0, 0]
+            }
+          }
+        ]
+      }
       this.chartInstance.setOption(adapterOption)
       //必须调用实例对象的resize方法，才会实时变化
       this.chartInstance.resize()
+    },
+    // 区域缩放定时器
+    startInterval() {
+      if (this.timerId) {
+        clearInterval(this.timerId)
+      }
+
+      this.timerId = setInterval(() => {
+        this.startValue++
+        this.endValue++
+        if (this.endValue > this.allData.length - 1) {
+          this.startValue = 0
+          this.endValue = 9
+        }
+        this.updateChart()
+      }, 2000)
     }
   }
 }
